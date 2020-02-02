edd44b411e5f90e9bd2ccbd795700def5c807654
leandrosoares <leandrosoares@gmail.com>
2020-02-02 16:20:20 -0300

    correcao script deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -11,5 +11,5 @@ docker push leandrosoares/multi-worker:$SHA
 
 kubectl apply -f k8s
 kubectl set image deployments/server-deployment server=leandrosoares/multi-server:$SHA
-kubectl set image deployments/client-deployment server=leandrosoares/multi-client:$SHA
-kubectl set image deployments/worker-deployment server=leandrosoares/multi-worker:$SHA
+kubectl set image deployments/client-deployment client=leandrosoares/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=leandrosoares/multi-worker:$SHA
